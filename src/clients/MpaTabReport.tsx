df404467b12e654fbc69be05048081bd52929ca1
Abby Vath Meyer <avmey@ucsb.edu>
2024-02-16 21:28:44 +0000

    Simple report printing #9

--- src/clients/MpaTabReport.tsx
+++ src/clients/MpaTabReport.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { SegmentControl, ReportPage } from "@seasketch/geoprocessing/client-ui";
 import ViabilityPage from "../components/ViabilityPage";
 import RepresentationPage from "../components/RepresentationPage";
 import KeyHabitatPage from "../components/KeyHabitatPage";
 import { useTranslation } from "react-i18next";
 import { Translator } from "../components/TranslatorAsync";
-
-const enableAllTabs = false;
+import { Printer, PrinterFill, Download } from "@styled-icons/bootstrap";
+import ReactToPrint from "react-to-print";
 
 const MpaTabReport = () => {
   const { t } = useTranslation();
@@ -20,24 +20,86 @@ const MpaTabReport = () => {
     { id: keyHabitatId, label: t("Key Habitat") },
   ];
   const [tab, setTab] = useState<string>(viabilityId);
+  const [enableAllTabs, setEnableAllTabs] = useState<boolean>(false);
+  const [showSegmentControl, setShowSegmentControl] = useState<boolean>(true);
+
+  const printRef = useRef(null);
   return (
     <>
-      <div style={{ marginTop: 5 }}>
-        <SegmentControl
-          value={tab}
-          onClick={(segment) => setTab(segment)}
-          segments={segments}
+      {enableAllTabs ? (
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          <ReactToPrint
+            trigger={() => (
+              <Download
+                size={18}
+                color="#999"
+                title="Download reports"
+                style={{
+                  margin: "10px 5px 5px 5px",
+                  cursor: "pointer",
+                  marginLeft: "auto",
+                }}
+              />
+            )}
+            content={() => printRef.current}
+          />
+          <PrinterFill
+            size={18}
+            color="#999"
+            title="Return to Standard View"
+            style={{
+              margin: "10px 5px 5px 5px",
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              setEnableAllTabs(false);
+              setShowSegmentControl(true);
+            }}
+          />
+        </div>
+      ) : (
+        <Printer
+          size={18}
+          color="#999"
+          title="Print View"
+          style={{
+            float: "right",
+            position: "relative",
+            margin: "10px 5px 5px 5px",
+            cursor: "pointer",
+          }}
+          onClick={() => {
+            setEnableAllTabs(true);
+            setShowSegmentControl(false);
+          }}
         />
+      )}
+
+      {showSegmentControl && (
+        <div style={{ marginTop: 5 }}>
+          <SegmentControl
+            value={tab}
+            onClick={(segment) => setTab(segment)}
+            segments={segments}
+          />
+        </div>
+      )}
+
+      <div ref={printRef}>
+        <ReportPage hidden={!enableAllTabs && tab !== viabilityId}>
+          <ViabilityPage geographyId={geographyId} />
+        </ReportPage>
+        <ReportPage hidden={!enableAllTabs && tab !== representationId}>
+          <RepresentationPage geographyId={geographyId} />
+        </ReportPage>
+        <ReportPage hidden={!enableAllTabs && tab !== keyHabitatId}>
+          <KeyHabitatPage geographyId={geographyId} />
+        </ReportPage>
       </div>
-      <ReportPage hidden={!enableAllTabs && tab !== viabilityId}>
-        <ViabilityPage geographyId={geographyId}/>
-      </ReportPage>
-      <ReportPage hidden={!enableAllTabs && tab !== representationId}>
-        <RepresentationPage geographyId={geographyId}/>
-      </ReportPage>
-      <ReportPage hidden={!enableAllTabs && tab !== keyHabitatId}>
-        <KeyHabitatPage geographyId={geographyId}/>
-      </ReportPage>
     </>
   );
 };
