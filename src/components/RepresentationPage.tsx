16a13a7c288ee783f22e627b2b0e4965249724a1
Abby Vath Meyer <avmey@ucsb.edu>
2024-04-03 18:25:35 +0000

    nit

--- src/components/RepresentationPage.tsx
+++ src/components/RepresentationPage.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BathymetryCard } from "./BathymetryCard";
-// import { Geomorphology } from "./Geomorphology";
+import { Geomorphology } from "./Geomorphology";
 import { ReportProps } from "../util/ReportProp";
 
 const ReportPage: React.FunctionComponent<ReportProps> = (props) => {
   return (
     <>
       <BathymetryCard printing={props.printing} />
-      {/* <Geomorphology printing={props.printing} /> */}
+      <Geomorphology printing={props.printing} />
     </>
   );
 };
--- src/components/ViabilityPage.tsx
+++ src/components/ViabilityPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { SizeCard } from "./SizeCard";
 import { SketchAttributesCard } from "@seasketch/geoprocessing/client-ui";
 import { ProtectionCard } from "./ProtectionCard";
-// import { HumanStressors } from "./HumanStressors";
+import { HumanStressors } from "./HumanStressors";
 import { ReportProps } from "../util/ReportProp";
 import { OusDemographics } from "./OusDemographic";
 
@@ -11,7 +11,7 @@ const ReportPage: React.FunctionComponent<ReportProps> = (props) => {
     <>
       <ProtectionCard printing={props.printing} />
       <SizeCard printing={props.printing} />
-      {/* <HumanStressors printing={props.printing} /> */}
+      <HumanStressors printing={props.printing} />
       <OusDemographics />
       {!props.printing && <SketchAttributesCard autoHide />}
     </>
